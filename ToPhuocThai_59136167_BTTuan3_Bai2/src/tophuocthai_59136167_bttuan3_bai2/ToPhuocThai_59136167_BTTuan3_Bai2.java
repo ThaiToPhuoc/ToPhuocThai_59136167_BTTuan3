d04ec87dd0aae5eb13df1b8c3044bd892bb8b4d7
Thaitophuoc <tophuocthai@gmail.com>
2020-04-16 11:29:43 +0700

    Hoàn chỉnh bải 2

--- ToPhuocThai_59136167_BTTuan3_Bai2/src/tophuocthai_59136167_bttuan3_bai2/ToPhuocThai_59136167_BTTuan3_Bai2.java
+++ ToPhuocThai_59136167_BTTuan3_Bai2/src/tophuocthai_59136167_bttuan3_bai2/ToPhuocThai_59136167_BTTuan3_Bai2.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package tophuocthai_59136167_bttuan3_bai2;
-
+import Hanghoa.HangHoa;
+import Giohang.GioHang;
+import Thanhtoan.IThanhToan;
+import Thanhtoan.ThanhToanCOD;
+import Thanhtoan.ThanhToanOnline;
 /**
  *
  * @author tophu
@@ -15,7 +19,17 @@ public class ToPhuocThai_59136167_BTTuan3_Bai2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        HangHoa HH1 = new HangHoa("Tivi", 1000000, "Tivi kỹ thuất số");
+        HangHoa HH2 = new HangHoa("Honda", 2000000, "Xe máy");
+        GioHang GH = new GioHang();
+        GH.themHH(HH1);
+        GH.themHH(HH2);
+        IThanhToan TTOnline = new ThanhToanOnline();
+        GH.setHinhThucTT(TTOnline);
+        System.out.println("Tổng tiền online: " + GH.thanhToan(GH.tinhtien()));
+        
+        IThanhToan TTCOD = new  ThanhToanCOD();
+        GH.setHinhThucTT(TTCOD);
+        System.out.println("Tổng tiền COD: " + GH.thanhToan(GH.tinhtien()));
     }
-    
 }
