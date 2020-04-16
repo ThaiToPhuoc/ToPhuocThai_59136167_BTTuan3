0350a9c2adb43f68bc1d070f1c00313ad8030096
Thaitophuoc <tophuocthai@gmail.com>
2020-04-16 20:40:42 +0700

    Sửa bài 3

--- ToPhuocThai_59136167_BTTuan3_Bai3/src/tophuocthai_59136167_bttuan3_bai3/ToPhuocThai_59136167_BTTuan3_Bai3.java
+++ ToPhuocThai_59136167_BTTuan3_Bai3/src/tophuocthai_59136167_bttuan3_bai3/ToPhuocThai_59136167_BTTuan3_Bai3.java
@@ -4,7 +4,15 @@
  * and open the template in the editor.
  */
 package tophuocthai_59136167_bttuan3_bai3;
-
+import QLSV.QLSV;
+import Sinhvien.SinhVien;
+import Isosanh.ISoSanh;
+import Isosanh.SoSanhTheoDiem;
+import java.util.Date;
+import Isosanh.SoSanhTheoDiem;
+import Isosanh.SoSanhTheoTen;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 /**
  *
  * @author tophu
@@ -14,8 +22,31 @@ public class ToPhuocThai_59136167_BTTuan3_Bai3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParseException {
+        QLSV QLDS = new QLSV();
+        SinhVien SV1 = new SinhVien("Tô Phước Thái",new SimpleDateFormat("dd/MM/yy").parse("04/11/1991"), 9);
+        SinhVien SV2 = new SinhVien("Dương Thị Mai",new SimpleDateFormat("dd/MM/yy").parse("18/10/1998"), 7);
+        SinhVien SV3 = new SinhVien("Lê Thế Dũng",new SimpleDateFormat("dd/MM/yy").parse("14/05/1998"), 10);
+        
+        QLDS.them(SV1);
+        QLDS.them(SV2);
+        QLDS.them(SV3);
+        
+        System.out.println("\n Danh sách sinh viên lúc đầu:");
+        QLDS.inDS();
+        
+        ISoSanh sstheodiem = new SoSanhTheoDiem();
+        ISoSanh sstheoten = new SoSanhTheoTen(); 
+        
+        QLDS.setSoSanh(sstheoten);
+        QLDS.sapXep();
+        System.out.println("\n Danh sách sinh viên sắp xếp theo tên: ");
+        QLDS.inDS();
+        
+        QLDS.setSoSanh(sstheodiem);
+        QLDS.sapXep();
+        System.out.println("\n Danh sách sinh viên sắp xếp theo điểm: ");
+        QLDS.inDS();
     }
     
 }
